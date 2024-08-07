14b21864698d5ccc1fb2c508cf50fa8766b18ff5
JaguarsCodehub <techbrains21@gmail.com>
2024-08-07 16:22:26 +0530

    Feature: BottomBar Color

--- src/app/(member)/_layout.tsx
+++ src/app/(member)/_layout.tsx
@@ -3,7 +3,7 @@ import { Tabs } from 'expo-router';
 
 export default function TabLayout() {
     return (
-        <Tabs screenOptions={{ tabBarActiveTintColor: 'blue', headerShown: true, }}>
+        <Tabs screenOptions={{ tabBarActiveTintColor: '#A67B5B', headerShown: true, }}>
             <Tabs.Screen
                 name="dashboard/dashboard"
                 options={{
